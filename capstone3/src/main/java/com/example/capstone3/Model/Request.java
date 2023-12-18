da13f3d6fd17b4f16bab5dc9fdcedd7f4cb0f57b
AreejAbdulaziz <148686069+AreejAbdulaziz@users.noreply.github.com>
2023-12-18 13:43:34 +0300

    Add files via upload

---
+++ capstone3/src/main/java/com/example/capstone3/Model/Request.java
@@ -0,0 +1,25 @@
+package com.example.capstone3.Model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Request {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String status="pending";
+
+    @ManyToOne
+    @JoinColumn(name = "member_id",referencedColumnName = "id")
+    @JsonIgnore
+    private Member member;
+}
